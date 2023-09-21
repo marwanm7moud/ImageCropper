057e59cc195c1127e3565c0575c100a1ce71731d
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-09-21 20:20:08 +0300

    make function to crop and show the image

--- cropper/src/commonMain/kotlin/com/awesome/cropper/cropper/ImageCropper.kt
+++ cropper/src/commonMain/kotlin/com/awesome/cropper/cropper/ImageCropper.kt
@@ -1,38 +1,22 @@
 package com.awesome.cropper.cropper
 
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.calculatePan
 import androidx.compose.foundation.gestures.detectTransformGestures
-import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement.SpaceBetween
 import androidx.compose.material.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalViewConfiguration
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.round
+import com.awesome.cropper.CroppingManager
 import com.awesome.cropper.getScreenSize
 import io.kamel.core.getOrNull
 import io.kamel.core.utils.File
@@ -41,26 +25,39 @@ import io.kamel.image.asyncPainterResource
 
 
 @Composable
-fun ImageCropper() {
+fun ImageCropper(
+    imagePath:String = "C:\\Users\\hp\\Desktop\\4t0oBFrJyweYPt0hocW6RUa0b6H.jpg"
+) {
     var croppingRectSize by remember { mutableStateOf(Size(200f, 200f)) }
     var croppingRectPosition by remember { mutableStateOf(Offset(50f, 50f)) }
 
     var isCroppingRectMoving by remember { mutableStateOf(false) }
     var touchOffset by remember { mutableStateOf(Offset(0f, 0f)) }
 
-    var image = File("C:\\Users\\hp\\Desktop\\4t0oBFrJyweYPt0hocW6RUa0b6H.jpg")
+    // Create a mutable state to hold the cropped image
+    var croppedImage by remember { mutableStateOf<ImageBitmap?>(null) }
+
+    // Function to crop the image
+    fun cropAndShowImage() {
+        // Calculate the cropping rectangle dimensions in pixels
+        val x = croppingRectPosition.x.toInt()
+        val y = croppingRectPosition.y.toInt()
+        val width = croppingRectSize.width.toInt()
+        val height = croppingRectSize.height.toInt()
+        
+        val croppedImageBitmap = CroppingManager().cropImage(imagePath, x, y, width, height)
+        croppedImage = croppedImageBitmap
+    }
 
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        // Display the image
         KamelImage(
-            resource = asyncPainterResource(data = "https://image.tmdb.org/t/p/original/4t0oBFrJyweYPt0hocW6RUa0b6H.jpg"),
+            resource = asyncPainterResource(data = File(imagePath)),
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
         )
-
         // Overlay cropping rectangles
         Box(
             modifier = Modifier
