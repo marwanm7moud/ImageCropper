3ffb3a131b3c971306a1d23032e8285121c2859a
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-09-22 13:49:43 +0300

    make the cropping rectangle does not move out of the image

--- cropper/src/commonMain/kotlin/com/awesome/cropper/cropper/ImageCropper.kt
+++ cropper/src/commonMain/kotlin/com/awesome/cropper/cropper/ImageCropper.kt
@@ -1,11 +1,14 @@
 package com.awesome.cropper.cropper
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -35,14 +38,18 @@ fun ImageCropper(
     var isCroppingRectMoving by remember { mutableStateOf(false) }
     val touchOffset by remember { mutableStateOf(Offset(0f, 0f)) }
 
+    var croppedImage by remember { mutableStateOf<ImageBitmap?>(null) }
+
+    val imageSize = 800.dp
+
     Box(
         modifier = Modifier.fillMaxSize().background(Color.Red)
     ) {
         KamelImage(
             resource = asyncPainterResource(data = File(imagePath)),
             contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Fit
+            modifier = Modifier.size(imageSize),
+            contentScale = ContentScale.Crop
         )
 
         Canvas(
@@ -57,11 +64,19 @@ fun ImageCropper(
                             // Check if touch is within the cropping rectangle
                             touchOffset.x >= 0 && touchOffset.x <= croppingRectSize.width &&
                                     touchOffset.y >= 0 && touchOffset.y <= croppingRectSize.height -> {
-                                // The user is touching and dragging the cropping rectangle
-                                croppingRectPosition = Offset(
-                                    croppingRectPosition.x + pan.x,
-                                    croppingRectPosition.y + pan.y
-                                )
+
+                                val maxX =
+                                    imageSize.roundToPx() - croppingRectSize.width.dp.roundToPx()
+                                val maxY =
+                                    imageSize.roundToPx() - croppingRectSize.height.dp.roundToPx()
+
+                                // Check if the new position is within the boundaries
+                                val newX =
+                                    (croppingRectPosition.x + pan.x).coerceIn(0f, maxX.toFloat())
+                                val newY =
+                                    (croppingRectPosition.y + pan.y).coerceIn(0f, maxY.toFloat())
+
+                                croppingRectPosition = Offset(newX, newY)
                                 isCroppingRectMoving = true
                             }
                             // The user is not touching the cropping rectangle
