352458571ac9bb128ec7191a611281b115c02930
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-09-23 17:33:27 +0300

    fix cropping in desktop

--- cropper/src/commonMain/kotlin/com/awesome/cropper/CroppingManager.kt
+++ cropper/src/commonMain/kotlin/com/awesome/cropper/CroppingManager.kt
@@ -1,7 +1,14 @@
 package com.awesome.cropper
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.ImageBitmap
 
 expect class CroppingManager() {
-    fun cropImageByImageBitmap(image: ImageBitmap, x: Int, y: Int, width: Int, height: Int): ImageBitmap
+    fun cropImageByImageBitmap(
+        image: ImageBitmap,
+        cropPosition:Offset,
+        cropSize: Size,
+        windowSize:Size,
+    ): ImageBitmap
 }
--- cropper/src/desktopMain/kotlin/com/awesome/cropper/CroppingManager.kt
+++ cropper/src/desktopMain/kotlin/com/awesome/cropper/CroppingManager.kt
@@ -1,25 +1,34 @@
 package com.awesome.cropper
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toAwtImage
 import androidx.compose.ui.graphics.toComposeImageBitmap
+import java.awt.Image
 import java.io.IOException
-import javax.imageio.ImageIO
 
 actual class CroppingManager actual constructor() {
     actual fun cropImageByImageBitmap(
         image: ImageBitmap,
-        x: Int,
-        y: Int,
-        width: Int,
-        height: Int
+        cropPosition: Offset,
+        cropSize: Size,
+        windowSize: Size,
     ): ImageBitmap {
         try {
+            val heightRatio =
+                (image.height / windowSize.height)
+            val widthRatio = (image.width / windowSize.width)
+
             // Convert the ImageBitmap to a BufferedImage
             val originalImage = image.toAwtImage()
-
             // Crop the BufferedImage
-            val croppedImage = originalImage.getSubimage(x, y, width, height)
+            val croppedImage = originalImage.getSubimage(
+                (cropPosition.x.toInt() * widthRatio ).toInt(),
+                (cropPosition.y.toInt() * heightRatio).toInt(),
+                (cropSize.width.toInt() * widthRatio).toInt(),
+                (cropSize.height.toInt() * heightRatio).toInt()
+            )
 
             // Convert the cropped BufferedImage back to an ImageBitmap
             return croppedImage.toComposeImageBitmap()
